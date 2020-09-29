ad8951d913a8ddd2f1e45f456dc64510baddde4e
tanveer99 <tanveerpannu99@gmail.com>
2020-09-30 11:41:15 +1300

    Add project files.

---
+++ ProjectManagementSystem/Controllers/ContractsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjectManagementSystem.Models;
+
+namespace ProjectManagementSystem.Controllers
+{
+    public class ContractsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        //This is used to get contract details list
+        // GET: Contracts
+        public ActionResult Index()
+        {
+            return View(db.Contracts.ToList());
+        }
+
+        //This is used to get contract details 
+        // GET: Contracts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contract contract = db.Contracts.Find(id);
+            if (contract == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contract);
+        }
+
+ 
+        // GET: Contracts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //This is used to save contract details 
+        // POST: Contracts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,FromDate,ToDate,OptionYears")] Contract contract)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contracts.Add(contract);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contract);
+        }
+
+        // GET: Contracts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contract contract = db.Contracts.Find(id);
+            if (contract == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contract);
+        }
+
+        //This is used to update contract details
+        // POST: Contracts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,FromDate,ToDate,OptionYears")] Contract contract)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contract).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contract);
+        }
+
+        // GET: Contracts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contract contract = db.Contracts.Find(id);
+            if (contract == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contract);
+        }
+
+        //This is used to delete contract details 
+        // POST: Contracts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contract contract = db.Contracts.Find(id);
+            db.Contracts.Remove(contract);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProjectManagementSystem/Controllers/ProjectTasksController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjectManagementSystem.Models;
+
+namespace ProjectManagementSystem.Controllers
+{
+    public class ProjectTasksController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        //This is used  to get tasks list 
+        // GET: ProjectTasks
+        public ActionResult Index()
+        {
+            var projectTasks = db.ProjectTasks.Include(p => p.Project);
+            return View(projectTasks.ToList());
+        }
+
+        //This is used to get task details
+        // GET: ProjectTasks/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProjectTask projectTask = db.ProjectTasks.Find(id);
+            if (projectTask == null)
+            {
+                return HttpNotFound();
+            }
+            return View(projectTask);
+        }
+
+        //This is used to get create view
+        // GET: ProjectTasks/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProjectID = new SelectList(db.Projects, "ID", "Name");
+            return View();
+        }
+
+        //This is used to create new tasks
+        // POST: ProjectTasks/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,ProjectID")] ProjectTask projectTask)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ProjectTasks.Add(projectTask);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProjectID = new SelectList(db.Projects, "ID", "Name", projectTask.ProjectID);
+            return View(projectTask);
+        }
+
+        // GET: ProjectTasks/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProjectTask projectTask = db.ProjectTasks.Find(id);
+            if (projectTask == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProjectID = new SelectList(db.Projects, "ID", "Name", projectTask.ProjectID);
+            return View(projectTask);
+        }
+
+        //This is used to update tasks
+        // POST: ProjectTasks/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,ProjectID")] ProjectTask projectTask)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(projectTask).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProjectID = new SelectList(db.Projects, "ID", "Name", projectTask.ProjectID);
+            return View(projectTask);
+        }
+
+        // GET: ProjectTasks/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProjectTask projectTask = db.ProjectTasks.Find(id);
+            if (projectTask == null)
+            {
+                return HttpNotFound();
+            }
+            return View(projectTask);
+        }
+
+        //This is used to delete tasks
+        // POST: ProjectTasks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ProjectTask projectTask = db.ProjectTasks.Find(id);
+            db.ProjectTasks.Remove(projectTask);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
